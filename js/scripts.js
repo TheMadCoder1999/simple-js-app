5fd3702c5d7fcd658502c4677f3b4fd4fce73476
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-03-05 20:03:00 -0500

    replaced old code with arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,32 @@
-alert('Hello World');
-let favoriteFood = 'pasta';
+var pokemonRepository = (function () {
+    let repository = [
+      {
+        name: 'Pikachu',
+        height: 0.4,
+        type: 'electric',
+        abilities: ['static', 'lightningrod']
+      },
+  
+      {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison'],
+        abilities: ['chlorophyll', 'over-grow']
+      },
+  
+      {
+        name: 'Charmander',
+        height: 0.6,
+        type: 'fire',
+        abilities: ['blaze', 'solar-power']
+      },
+  
+      {
+        name: 'Squirtle',
+        height: 0.5,
+        type: 'water',
+        abilities: ['rain-dish', 'torrent']
+      },
+    ]
+    }
+);
