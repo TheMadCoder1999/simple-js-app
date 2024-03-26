7b08bc2ebf72909c90154002d39495fdfd76a312
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-03-25 22:48:05 -0400

    made changes to fix errors allowing the buttons to show on the index page
    1- In the add function, you're checking if "types" is in the pokemon object, but it should be "type". Additionally, you're trying to push to repository, which isn't defined. You should push to pokemonList instead.
    
    2- In the addListItem function, you're redefining pokemonList locally, which overrides the outer scope variable. You should use a different variable name to avoid this conflict.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,70 @@
-let pokemonRepository=(function () {
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
   {
-    name: 'Pikachu',
-    height: 0.4,
-    type: 'electric',
-    abilities: ['static', 'lightningrod']
+  name: "Pikachu",
+  height: 0.4,
+  type: "electric",
+  abilities: ["static", "lightningrod"],
   },
-
+  
   {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison'],
-    abilities: ['chlorophyll', 'over-grow']
+  name: "Bulbasaur",
+  height: 0.7,
+  type: ["grass", "poison"],
+  abilities: ["chlorophyll", "over-grow"],
   },
-
+  
   {
-    name: 'Charmander',
-    height: 0.6,
-    type: 'fire',
-    abilities: ['blaze', 'solar-power']
+  name: "Charmander",
+  height: 0.6,
+  type: "fire",
+  abilities: ["blaze", "solar-power"],
   },
-
+  
   {
-    name: 'Squirtle',
-    height: 0.5,
-    type: 'water',
-    abilities: ['rain-dish', 'torrent']
+  name: "Squirtle",
+  height: 0.5,
+  type: "water",
+  abilities: ["rain-dish", "torrent"],
   },
-];
-
-function add(pokemon) {
+  ];
+  
+  function add(pokemon) {
   if (
-    typeof pokemon === "object" &&
-    "name" in pokemon &&
-    "height" in pokemon &&
-    "types" in pokemon
+  typeof pokemon === "object" &&
+  "name" in pokemon &&
+  "height" in pokemon &&
+  "type" in pokemon
   ) {
-    repository.push(pokemon);
+  pokemonList.push(pokemon); // Changed 'repository' to 'pokemonList'
   } else {
-    console.log("pokemon is not correct");
+  console.log("pokemon is not correct");
   }
-}
-function getAll() {
-  return pokemonRepository;
-}
-
-function addListItem(pokemon){
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listpokemon = document.createElement("li");
+  }
+  function getAll() {
+  return pokemonList;
+  }
+  
+  function addListItem(pokemon) {
+  let listContainer = document.querySelector(".pokemon-list"); // Changed variable name to avoid conflict
+  let listItem = document.createElement("li");
   let button = document.createElement("button");
   button.innerText = pokemon.name;
   button.classList.add("button-class");
-  listpokemon.appendChild(button);
-  pokemonList.appendChild(listpokemon);
-}
-return {
+  listItem.appendChild(button);
+  listContainer.appendChild(listItem);
+  }
+  return {
   add: add,
   getAll: getAll,
-  addListItem: addListItem
-};
-})();
-
-pokemonRepository.add({ name: "Lopunny", height: 1.0, type: ["normal"] });
-
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-pokemonRepository.addListItem(pokemon);
-});
+  addListItem: addListItem,
+  };
+  })();
+  
+  pokemonRepository.add({ name: "Lopunny", height: 1.0, type: ["normal"] });
+  
+  console.log(pokemonRepository.getAll());
+  
+  pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+  });
